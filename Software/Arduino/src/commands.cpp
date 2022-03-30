f79bee203d534177a95aa71ca2623185559847a1
Alan Finger <afinger@goldenageradios.com>
2022-03-29 22:00:00 -0400

    Set up command processor structures.

---
+++ Software/Arduino/src/commands.cpp
@@ -0,0 +1,14 @@
+/* Command Processors
+*/
+
+#include "commands.h"
+char * process_sf(char * cmd_line) {
+    return (char *) "";
+
+}
+char * process_sb(char * cmd_line) {
+    return (char *) "";
+}
+char * process_gf(char * cmd_line) {
+    return (char *) "";
+}
--- Software/Arduino/src/commands.h
+++ Software/Arduino/src/commands.h
@@ -5,19 +5,26 @@ Software Controlled Radio - Command Definitions
 #ifndef COMMANDS_H
 #define COMMANDS_H
 
+typedef char* (* processor_p)(char * cmd_line) ;
+typedef char* processor_t(char * cmd_line) ;
+
+
 typedef struct {
-    char * name;
-    char * (*processor)(char * cmd_line);
+    char cmd[3];
+    processor_p processor;
 } command;
 
-char * process_sf(char * cmd_line);
-char * process_sb(char * cmd_line);
+processor_t process_sf;
+processor_t process_sb;
+processor_t process_gf;
+
 
 #define COMMAND_COUNT
 command command_list []= {
-    {"SF", process_sf },    //Set Frequency: SF,float_frequency,units
-    {"SB", process_sb },    //Set Band: SB,float_lower_limit, float_upper_limit, units
-}
+    {"SF", &process_sf },    //Set Frequency: SF,float_frequency (KHz)
+    {"SB", &process_sb },    //Set Band: SB,float_lower_limit, float_upper_limit 
+    {"GF", &process_gf}      //Get Current Frequency: GF. Returns float frequency in KHz
+};
 
 
 #endif
